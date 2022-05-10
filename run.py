de143bb52d5427afcc61243ac94b616e3e075b6c
kwalczak80 <christopher.walczak@gmail.com>
2022-05-10 19:31:17 +0000

    create class for colors that will be used in the game

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Melting Gerry game
+import os
+import sys
+import random
+from word_categories import animal_words
+from word_categories import job_and_occupation_words
+from word_categories import fruit_words
+from word_categories import food_words
+from word_categories import color_words
+from snowman_gerry import display_snowman
+
+width = os.get_terminal_size().columns
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
